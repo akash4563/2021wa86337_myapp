d4635a9799a1d95352eb0c48ba2b917fd8a7ef8c
akash4563 <das.akash4563@gmail.com>
2025-06-12 14:17:38 +0530

    Deleted unnecessary files

--- src/main/java/com/saco/2021wa86337_myapp/App.java
+++
@@ -1,9 +0,0 @@
-package com.saco.2021wa86337_myapp;
-public class App {
-   public static void main(String[] args) {
-       System.out.println("Hello, Saco IT Delivery!");
-   }
-   public int add(int a, int b) {
-       return a + b;
-   }
-
