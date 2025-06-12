5cec79272fba19415ceb29007d776036dd486f9d
akash4563 <das.akash4563@gmail.com>
2025-06-12 14:20:12 +0530

    Added new files

---
+++ src/main/java/com/saco/myapp2021wa86337/App.java
@@ -0,0 +1,9 @@
+package com.saco.myapp2021wa86337;
+public class App {
+   public static void main(String[] args) {
+       System.out.println("Hello, Saco IT Delivery!");
+   }
+   public int add(int a, int b) {
+       return a + b;
+   }
+}
---
+++ src/test/java/com/saco/myapp2021wa86337/AppTest.java
@@ -0,0 +1,10 @@
+package com.saco.myapp2021wa86337;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class AppTest {
+   @Test
+   public void testAdd() {
+       App app = new App();
+       assertEquals(5, app.add(2, 3));
+   }
+}
